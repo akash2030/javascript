0175a5d86c4c45aa015c4b7cba19bf43a2fe3da2
MD Akash(Sopon) <68960056+akash2030@users.noreply.github.com>
2020-09-14 20:23:21 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var count=0;
+
+$("h2").text("Start Play Player");
+
+$(".gameroom").click(function(){
+    count= count+1;
+    //$(this).text("X");
+    //$(this).text(count);
+
+
+    if(count%2==0){
+        $(this).text("0");
+    }
+
+    else{
+        $(this).text("X");
+    }
+    
+})
